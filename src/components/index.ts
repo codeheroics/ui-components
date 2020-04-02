307e32de05600b8a3b4fee86d539745afb39b09a
pducolin <paola.ducolin@dashlane.com>
2020-04-02 19:48:03 +0200

    refactor: move textinput folder to text-input to respect convention

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,5 +1,5 @@
 export { default as DropdownMenu } from './dropdown/DropdownMenu';
 export { default as DropdownElement } from './dropdown/DropdownElement';
 export { default as Tooltip } from './tooltip/Tooltip';
-export { TextInput } from './textinput/TextInput';
+export { TextInput } from './text-input/TextInput';
 export * from './tooltip/types';
